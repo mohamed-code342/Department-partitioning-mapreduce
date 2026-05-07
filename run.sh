6e3a26399c88705e37ad5c8efa93a04c769d9db0
Mohamed Saad <mahammadsaad74@gmail.com>
2026-05-07 15:36:22 +0300

    Modify run.sh for updated Hadoop job execution
    Updated Hadoop job command and input/output paths.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-hdfs dfs -rm -r /output
-
-hadoop jar target/department-partitioning.jar \
-com.department.driver.DepartmentDriver \
-/input /output
+hadoop jar Jar/departmentpartitioning.jar departmentpartitioning.DepartmentDriver \
+-Dyarn.app.mapreduce.am.staging-dir=/user/cloudera/tmp \
+-Dmapreduce.job.working.dir=/user/cloudera/tmp \
+/user/cloudera/input/department/employees_sample.txt \
+/user/cloudera/DepartmentDriver/result_sample
